f8d963b9e32bfa5f3090faefda1285d61ab123df
Mohammad Naqaweh <mohammad.naqaweh@gmail.com>
2023-11-03 02:16:07 +0300

    add getJobPosts route to express

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 import cors from 'cors';
 import express  from 'express';
+import { getJobs } from './getJobs.js';
 
 let app = express();
 app.use(cors());
+
+app.get('/getJobPosts', (req, res) => {
+  getJobs(req, res);
+})
 
 const server = app.listen("4000", () => {
   console.log("App is active");
