42fe07f0a15438eb7addf8fd7f40fa328141a046
Mohammad Naqaweh <mohammad.naqaweh@gmail.com>
2023-11-03 02:27:30 +0300

    Add getCoordinates Route

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 import cors from 'cors';
 import express  from 'express';
 import { getJobs } from './getJobs.js';
+import { getCoordinates } from './getCoordinates.js'
 
 let app = express();
 app.use(cors());
 
 app.get('/getJobPosts', (req, res) => {
   getJobs(req, res);
+})
+
+app.get('/getCoordinates' , (req, res) => {
+  getCoordinates(req, res);
 })
 
 const server = app.listen("4000", () => {
